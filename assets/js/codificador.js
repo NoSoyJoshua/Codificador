ec2171b76040b25520fb34d4de7c7bed35032ab2
NoSoyJoshua <quesadaleonjoshua@gmail.com>
2022-05-08 22:01:10 -0600

    Add warning and small changes to the layout

--- assets/js/codificador.js
+++ assets/js/codificador.js
@@ -1,15 +1,17 @@
 const encryptationCode = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 
 export function encrypt (text) {
+    let result = text.toLowerCase();
     for (let i = 0; i < encryptationCode.length; i++) {
-        if (text.includes(encryptationCode[i][0])) text = text.replaceAll(encryptationCode[i][0], encryptationCode[i][1]);
+        if (result.includes(encryptationCode[i][0])) result = result.replaceAll(encryptationCode[i][0], encryptationCode[i][1]);
     }
-    return text;
+    return result;
 }
 
 export function decrypt (text) {
+    let result = text.toLowerCase();
     for (let i = encryptationCode.length - 1; i >= 0; i = i - 1) {
-        if (text.includes(encryptationCode[i][1])) text = text.replaceAll(encryptationCode[i][1], encryptationCode[i][0]);
+        if (result.includes(encryptationCode[i][1])) result = result.replaceAll(encryptationCode[i][1], encryptationCode[i][0]);
     }
-    return text;
+    return result;
 }

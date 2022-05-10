6a8566cdfcc46cb4148d1abb7fd08eb911278ddf
NoSoyJoshua <quesadaleonjoshua@gmail.com>
2022-05-09 18:53:54 -0600

    Add functionality to the darkmode button and fix issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 import { encrypt, decrypt } from "./codificador.js";
 
+// HTML Elements
 const navbar = $("nav");
 const navbarButton = $(".navbar-mobile-button");
 const navbarButtonLayout = $(".navbar-mobile-button-layout");
@@ -9,6 +10,14 @@ const resultArea = $(".result-area");
 const encryptButton = $(".encrypt-button");
 const decryptButton = $(".decrypt-button");
 const copyButton = $(".copy-button");
+const darkmodeButton = $('#checkbox');
+
+// Color variables
+const varModes = ["--brightest-color", "--second-brightest-color", "--darkest-color", "--second-darkest-color", "--inbetween-color"]
+const modes = [["#FFFFFF", "#D9D9D9", "#353535", "#284B63", "#3C6E71"], ["#353535", "#284B63", "#3C6E71", "#D9D9D9", "#FFFFFF"]]
+
+// Other useful variables
+let mode = 0;
 
 navbarButton.click(function () {
     navbar.toggleClass("nav-unhide");
@@ -34,4 +43,21 @@ copyButton.click(function () {
     /* Copy the text inside the text field */
     navigator.clipboard.writeText(resultArea[0].innerText); 
     alert("¡Mensaje copiado!");    
+});
+
+darkmodeButton.click( () => {
+    mode = (mode + 1) % 2;
+
+    for (let i = 0; i < varModes.length; i++) {
+        document.documentElement.style.setProperty(varModes[i], modes[mode][i]);
+    }
+
+    logo.classList.toggle("darken-logo");
+})
+
+$(window).on("orientationchange",function(){
+    navbar.removeClass("nav-unhide");
+    navbarButton.removeClass("navbar-button-unhide");
+    console.log(!(navbarButtonLayout.hasClass("hidden")));
+    if (!(navbarButtonLayout.hasClass("hidden"))) navbarButtonLayout.addClass("hidden");
 });

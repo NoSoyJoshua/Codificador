7c6da6293d67057e915bf35b298047cbaac80bff
NoSoyJoshua <quesadaleonjoshua@gmail.com>
2022-05-09 18:13:22 -0600

    Change the layout of both mobile and computer versions

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,7 @@ import { encrypt, decrypt } from "./codificador.js";
 
 const navbar = $("nav");
 const navbarButton = $(".navbar-mobile-button");
+const navbarButtonLayout = $(".navbar-mobile-button-layout");
 const textArea = $(".text-area")[0];
 const resultContainer = $(".result-container");
 const resultArea = $(".result-area");
@@ -12,18 +13,25 @@ const copyButton = $(".copy-button");
 navbarButton.click(function () {
     navbar.toggleClass("nav-unhide");
     navbarButton.toggleClass("navbar-button-unhide");
-})
+    navbarButtonLayout.toggleClass("hidden");
+});
 
 encryptButton.click(function () {
     resultContainer.addClass("result-container-filled");
     copyButton.removeClass("hidden");
     resultArea.removeClass("hidden");
     resultArea[0].innerText = encrypt(textArea.value);
-})
+});
 
 decryptButton.click(function () {
     resultContainer.addClass("result-container-filled");
     copyButton.removeClass("hidden");
     resultArea.removeClass("hidden");
     resultArea[0].innerText = decrypt(textArea.value);
-})
+});
+
+copyButton.click(function () {
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(resultArea[0].innerText); 
+    alert("¡Mensaje copiado!");    
+});
